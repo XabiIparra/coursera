ddadb07b79e65fa6a64f6a91a45a44fead5971b7
Xabi <x.iparra7@gmail.com>
2017-03-05 16:06:09 +0100

    Assigment 1

---
+++ assignment1-starter-code/app.js
@@ -0,0 +1,31 @@
+(function () {
+'use strict';
+
+angular.module('LunchChecker', [])
+
+.controller('LunchCheckController', function ($scope) {
+  $scope.name = "";
+  $scope.totalValue = "";
+
+  $scope.myFunction = function () {
+    var totalNameValue = calculateNumericForString($scope.name);
+    if(totalNameValue<3){
+      var message = "Enjoy"
+    }else{
+      var message = "Too much!"
+    }
+    $scope.totalValue = totalNameValue;
+  };
+
+
+  function calculateNumericForString(string) {
+    var totalStringValue = string.split(,);
+
+
+    return totalStringValue.length;
+  }
+
+});
+
+
+})();
